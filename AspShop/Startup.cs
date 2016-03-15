4d9bf34943621c865fa3edd8083d32ab9e735380
a-einstein <aeinstein@xs4all.nl>
2016-03-15 16:58:27 +0100

    As from template.

---
+++ AspShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspShop.Startup))]
+namespace AspShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
