939284ee97f3663d6d88d71b61b3a59e049e0e89
a-einstein <aeinstein@xs4all.nl>
2016-03-17 19:47:24 +0100

    Got basic product overview running in a rough version.

---
+++ AspShop/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using AspShop.Models;
+using System.Web.Mvc;
+
+namespace AspShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            var viewModel = new ProductsViewModel();
+            viewModel.ReadFiltered();
+
+            return View(viewModel);
+        }
+
+        // GET: Product
+        // TODO Parameter.
+        public ActionResult Details()
+        {
+            // TODO
+            return null;
+        }
+    }
+}
---
+++ AspShop/Models/ProductsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using AspShop.ServiceClients.Products.ProductsService;
+using Common.DomainClasses;
+using System.Collections.Generic;
+
+namespace AspShop.Models
+{
+    public class ProductsViewModel
+    {
+        public ProductsViewModel()
+        {
+            Items = new List<ProductsOverviewObject>();
+        }
+
+        public List<ProductsOverviewObject> Items { get; set; }
+
+        public void ReadFiltered()
+        {
+            // HACK
+            var masterFilterValue = new ProductCategory() { Id = 1 };
+            var detailFilterValue = new ProductSubcategory() { Id = 1, ProductCategoryId = 1 };
+            var textFilterValue = "sil";
+            var result = ReadList(masterFilterValue, detailFilterValue, textFilterValue);
+
+            foreach (var item in result)
+            {
+                Items.Add(item);
+            }
+        }
+
+        private ProductsServiceClient productsServiceClient;
+
+        // TODO Check this.
+        protected ProductsServiceClient ProductsServiceClient
+        {
+            get
+            {
+                if (productsServiceClient == null)
+                    productsServiceClient = new ProductsServiceClient();
+
+                return productsServiceClient;
+            }
+        }
+
+        public IList<ProductsOverviewObject> ReadList(ProductCategory category, ProductSubcategory subcategory, string namePart)
+        {
+            var productOverview = ProductsServiceClient.GetProductsOverviewBy(
+                    category != null ? category.Id : null,
+                    subcategory != null ? subcategory.Id : null,
+                    namePart);
+
+            return productOverview;
+        }
+    }
+}
